28760642886a32ab1144059a998336eafdd3e0ad
Valkryn3 <159496206+Valkryn3@users.noreply.github.com>
2024-04-08 14:14:45 -0700

    Update script.js

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+var greeting = "Hello and welcome to my website!";
+var intro = "This site contains some of my favorite movies and games!";
+var el = document.getElementById('greeting');
+el.textContent = intro;

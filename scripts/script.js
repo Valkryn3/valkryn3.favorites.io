3f94e621e812bb8e584d47f3bd22e3276cacb94d
Valkryn3 <159496206+Valkryn3@users.noreply.github.com>
2024-04-15 19:22:23 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ var greeting = "Hello and welcome to my website! This site contains some of my f
 var el = document.getElementById('greeting');
 el.textContent = greeting;
 
+window.alert("OFFER: Email me your favorite movie or game and win a prize!");
+
   var expiryMsg; 
   var today;
   var elEnds;

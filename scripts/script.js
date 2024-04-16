9a7908d48a040d480720a1fd631597d632a211ee
Valkryn3 <159496206+Valkryn3@users.noreply.github.com>
2024-04-15 17:57:04 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,46 @@
 var greeting = "Hello and welcome to my website! This site contains some of my favorite movies and games!";
 var el = document.getElementById('greeting');
 el.textContent = greeting;
+
+(function() {
+var hotel = {
+  name = 'Park',
+  roomRate = 240,
+  discount: 15,
+  offerPrice: function(){
+    var offerRate = this.roomRate * ((100 - this.discount) / 100);
+    return offerRate;
+  }
+  }
+
+  var hotelName, roomRate, specialRate;
+  
+  hotelName = document.getElementById('hotelName');
+  roomRate = document.getElementById('roomRate');
+  specialRate = document.getElementById('specialRate');
+
+  hotelName.textContent = hptal.name;
+  roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
+  specialRate.textContent = '$' + hotel.offerPrice();
+
+  var expiryMsg;
+  var today;
+  var elEnda;
+
+  function offerExpires(today){
+    var weekFromToday, day, date, month, year, dayNames, monthNames;
+    weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+    dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    day = dayNames[weekFromToday.getDay()];
+    month = monthNames[weekFromToday.getMonth()];
+    year = weekFromToday.getFullYear()];
+    expiryMsg = 'Offer expires next ';
+    expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ' ' + ')';
+    return expiryMsg;
+  }
+  today = new Date();
+  elEnds = document.getElementById('offerEnds');
+  elEnds.innerHTML = offerExpires(today);
+}());
+  

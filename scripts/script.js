2f25fbf3900040beb8f01744b3722d34f49543d3
Valkryn3 <159496206+Valkryn3@users.noreply.github.com>
2024-04-15 19:06:42 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,37 +2,28 @@ var greeting = "Hello and welcome to my website! This site contains some of my f
 var el = document.getElementById('greeting');
 el.textContent = greeting;
 
-(function() {
-  var expiryMsg; // Message displayed to users
-  var today;     // Today's date
-  var elEnds;    // The element that shows the message about the offer ending
-
+  var expiryMsg; 
+  var today;
+  var elEnds;
   function offerExpires(today) {
-    // Declare variables within the function for local scope
     var weekFromToday, day, date, month, year, dayNames, monthNames;
 
-    // Add 7 days time (added in milliseconds)
     weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
 
-    // Create arrays to hold the names of days / months
     dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    // Collect the parts of the date to show on the page
     day = dayNames[weekFromToday.getDay()];
     date = weekFromToday.getDate();
     month = monthNames[weekFromToday.getMonth()];
     year = weekFromToday.getFullYear();
 
-    // Create the message
     expiryMsg = 'Offer expires next ';
     expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
     return expiryMsg;
   }
 
-  today = new Date();                             // Put today's date in variable
-  elEnds = document.getElementById('offerEnds');  // Get the offerEnds element
-  elEnds.innerHTML = offerExpires(today);         // Add the expiry message
+  today = new Date();
+  elEnds = document.getElementById('offerEnds'); 
+  elEnds.innerHTML = offerExpires(today);
 
-// Finish the immediately invoked function expression
-}());

b5171fed25bb08bfd7fcaf66778213730545a278
Valkryn3 <159496206+Valkryn3@users.noreply.github.com>
2024-04-23 11:19:50 -0700

    Delete scripts/script1.js

--- scripts/script1.js
+++
@@ -1,9 +0,0 @@
- var score = 75;    
-  var msg;            
-
-  if (score >= 50) {  
-    msg = 'If you love movies and video games, you have come to the right place!';
-  }
-
-  var el = document.getElementById('answer').innerHTML = msg;
-  

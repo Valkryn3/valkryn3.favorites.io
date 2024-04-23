1a6f9ef17674533350d657f01e127350aa0f0365
Valkryn3 <159496206+Valkryn3@users.noreply.github.com>
2024-04-22 21:09:53 -0700

    Create script1.js

---
+++ scripts/script1.js
@@ -0,0 +1,9 @@
+ var score = 75;    
+  var msg;            
+
+  if (score >= 50) {  
+    msg = 'If you love movies and video games, you have come to the right place!';
+  }
+
+  var el = document.getElementById('answer');
+  el.textContent = msg;

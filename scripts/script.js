1c82b09fbb1f1d3016daac04a7abc7e0ce5b87db
Valkryn3 <159496206+Valkryn3@users.noreply.github.com>
2024-04-22 21:09:27 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -27,12 +27,4 @@ el.textContent = greeting;
   elEnds = document.getElementById('offerEnds'); 
   elEnds.innerHTML = offerExpires(today);
 
-  var score = 75;    
-  var msg;            
-
-  if (score >= 50) {  
-    msg = 'If you love movies and video games, you have come to the right place!';
-  }
-
-  var el = document.getElementById('answer');
-  el.textContent = msg;
+ 
